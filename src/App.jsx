6193ca8ce8691551a51427aba2ff34710bb0a866
sukanta022 <sukantacb20@gmail.com>
2025-11-20 20:22:06 +0600

    added toggling features using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Navbar from './components/Navbar/Navbar'
 import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers'
+import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers'
 
 
 
@@ -14,15 +15,26 @@ const allPlayersPromise = async () => {
 
 function App() {
   const allPlayers = allPlayersPromise()
-
+  const[toggle, setToggle] = useState(false)
   return (
     <>
       <div>
         <Navbar></Navbar>
         <Banner></Banner>
-        <Suspense fallback={<span class="loading loading-spinner loading-lg"></span>}>
+        <div className='max-w-[1400px] mx-auto mt-10 flex justify-between items-center'>
+          <p className='text-2xl font-bold'>{toggle?"Available Players":"Selected Players"}</p>
+          <div className='flex '>
+            <button className={`btn rounded-l-xl border-r-0 ${toggle && "bg-[#E7FE29]"}`} onClick={()=> setToggle(true)}>Available</button>
+            <button className={`btn rounded-r-xl border-l-0 ${toggle || "bg-[#E7FE29]"}`} onClick={()=> setToggle(false)}>Selected (<span>0</span>)</button>
+          </div>
+        </div>
+        {
+          toggle ? <Suspense fallback={<span class="loading loading-spinner loading-lg"></span>}>
           <AvailablePlayers allPlayers={allPlayers}></AvailablePlayers>
-        </Suspense>
+        </Suspense> : <SelectedPlayers></SelectedPlayers>
+        }
+        
+        
       </div>
       
     </>
---
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div className='max-w-[1400px] mx-auto mt-10'>
+            <p>selected</p>
+        </div>
+    );
+};
+
+export default SelectedPlayers;
